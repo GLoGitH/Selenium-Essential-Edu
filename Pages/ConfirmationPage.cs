816eea5e1f368908eaa784a80033e9caf54a6741
GLoGitH <Gert.Longin@cronos.be>
2020-02-26 13:17:28 +0100

    Add project files.

---
+++ Pages/ConfirmationPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+//using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;      //for the WebDriverWait
+using SeleniumExtras.WaitHelpers;      //for ExpectedConditions
+
+
+
+namespace Selenium_Essential_LinkedIn.Pages
+{
+    public class ConfirmationPage
+    {
+        IWebDriver driver;
+
+        public ConfirmationPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        By alertCtrl = By.ClassName("alert");
+
+
+
+
+        public void waitForAlertMsgOnPage()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(alertCtrl));
+        }
+
+        public string getAlertBannerText(IWebDriver driver)
+        {
+            return driver.FindElement(alertCtrl).Text;
+        }
+
+    }
+}
---
+++ Pages/FormPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+//using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+//using OpenQA.Selenium.Support.UI;      //for the WebDriverWait
+//using SeleniumExtras.WaitHelpers;      //for ExpectedConditions
+
+
+
+
+namespace Selenium_Essential_LinkedIn.Pages
+{
+    public class FormPage
+    {
+        IWebDriver driver;
+
+
+        By datePicker = By.Id("datepicker");
+
+        public FormPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    
+        public void submitForm()
+        {
+            IWebElement firstNameField = driver.FindElement(By.Id("first-name"));
+            firstNameField.SendKeys("John");
+
+            IWebElement lastNameField = driver.FindElement(By.Id("last-name"));
+            lastNameField.SendKeys("Doe");
+
+            IWebElement JobTitleField = driver.FindElement(By.Id("job-title"));
+            JobTitleField.SendKeys("QA Engineer");
+
+            IWebElement eduLevelField = driver.FindElement(By.Id("radio-button-3"));
+            eduLevelField.Click();
+
+            IWebElement sexField = driver.FindElement(By.Id("checkbox-2"));
+            sexField.Click();
+
+            IWebElement ddlItem = driver.FindElement(By.CssSelector("option[value='3']"));
+            ddlItem.Click();
+
+            IWebElement dateField = driver.FindElement(datePicker);
+            dateField.SendKeys(String.Format("{0}/{1}/{2}", DateTime.Now.Month, DateTime.Now.Day + 1, DateTime.Now.Year));
+            dateField.SendKeys(Keys.Enter); //close the calender popup 
+
+            IWebElement submitBtn = driver.FindElement(By.CssSelector(".btn.btn-lg.btn-primary"));
+            submitBtn.Click();
+        }
+    }
+}
